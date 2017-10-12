602edf69dc1078e072f474737cef5d63b31a0f71
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2017-10-12 10:59:45 -0400

    created server.js and connected to mongoose

---
+++ server.js
@@ -0,0 +1,36 @@
+// Import variables from .env file
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+mongoose.Promise = global.Promise
+
+//Create a new app using express 
+const app = express()
+
+//Connect to MongoDB and set up messages for when
+// Mongo connects succesffuly or errors out
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+const connection = mongoose.connection;
+connection.on('connected', () => {
+    console.log('Successfully connected to MongoDB')
+})
+// If the connection throws an error
+connection.on('error', (err) => {
+    console.log('MongoDB error: ', err);
+  }) 
+
+//Inject middleware
+app.use(bodyParser.json());
+
+//Index
+app.get('/', (req,res) => {
+  res.send('Hello world!')
+})
+
+
+//Set the app to listen on a specific port
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log("App listening on port: ", PORT)
+})
